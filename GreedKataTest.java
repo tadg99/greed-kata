b64da24e1e7621d7fe7efef627734691b78f0265
tadg99 <tadg99@cs.washington.edu>
2021-03-23 12:09:26 -0700

    Added edge case tests

--- GreedKataTest.java
+++ GreedKataTest.java
@@ -36,6 +36,24 @@ public class GreedKataTest {
         } else {
             System.out.println("Failed test 'testBestScoreDifferentOrder'");
         }
+
+        if (testBestScoreBadInput1()) {
+            System.out.println("Passed test 'testBestScoreBadInput1'");
+        } else {
+            System.out.println("Failed test 'testBestScoreBadInput1'");
+        }
+
+        if (testBestScoreBadInput2()) {
+            System.out.println("Passed test 'testBestScoreBadInput2'");
+        } else {
+            System.out.println("Failed test 'testBestScoreBadInput2'");
+        }
+
+        if (testBestScoreBadInput3()) {
+            System.out.println("Passed test 'testBestScoreBadInput3'");
+        } else {
+            System.out.println("Failed test 'testBestScoreBadInput3'");
+        }
     }
 
     //Tests bestScore method on example 1 from spec
@@ -80,5 +98,37 @@ public class GreedKataTest {
         return GreedKata.bestScore(roll1) == GreedKata.bestScore(roll2);
     }
 
+    //Tests bestScore method on an input array of length 6
+    public static boolean testBestScoreBadInput1() {
+        int[] roll = new int[]{1, 1, 1, 5, 4, 1};
+        try {
+            GreedKata.bestScore(roll);
+            return false;
+        } catch (IllegalArgumentException e) {
+            return true;
+        }
+    }
+
+    //Tests bestScore method on an input array with value less than 1
+    public static boolean testBestScoreBadInput2() {
+        int[] roll = new int[]{1, 1, 0, 5, 4};
+        try {
+            GreedKata.bestScore(roll);
+            return false;
+        } catch (IllegalArgumentException e) {
+            return true;
+        }
+    }
+
+    //Tests bestScore method on an input array with value greater than 6
+    public static boolean testBestScoreBadInput3() {
+        int[] roll = new int[]{1, 1, 1, 5, 7};
+        try {
+            GreedKata.bestScore(roll);
+            return false;
+        } catch (IllegalArgumentException e) {
+            return true;
+        }
+    }
     
 }
